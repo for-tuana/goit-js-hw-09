c17fa05805a3f9482313678e3f0c6096d0a432f2
for-tuana <tuana_albayrak61@hotmail.com>
2025-03-25 15:02:15 +0300

    update last vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,66 @@
+// import { defineConfig } from 'vite';
+// import { glob } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import SortCss from 'postcss-sort-media-queries';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: glob.sync('./src/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: chunkInfo => {
+//             if (chunkInfo.name === 'commonHelpers') {
+//               return 'commonHelpers.js';
+//             }
+//             return '[name].js';
+//           },
+//           assetFileNames: assetInfo => {
+//             if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+//               return '[name].[ext]';
+//             }
+//             return 'assets/[name]-[hash][extname]';
+//           },
+//         },
+//         external: ['simplelightbox'],
+//       },
+//       base: '/goit-js-hw-09/',
+//       outDir: '../dist',
+//       emptyOutDir: true,
+//     },
+//     plugins: [
+//       injectHTML(),
+//       FullReload(['./src/**/**.html']),
+//       SortCss({
+//         sort: 'mobile-first',
+//       }),
+//     ],
+//   };
+// });
+
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-
 export default defineConfig(({ command }) => {
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-js-hw-09/', // Base URL'i buraya taşıdım
     build: {
       sourcemap: true,
       rollupOptions: {
@@ -33,9 +84,8 @@ export default defineConfig(({ command }) => {
             return 'assets/[name]-[hash][extname]';
           },
         },
-        external: ['simplelightbox'],
+        // external: ['simplelightbox'], // Bu satırı kaldırdım
       },
-      base: '/goit-js-hw-09/',
       outDir: '../dist',
       emptyOutDir: true,
     },
